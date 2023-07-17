f55f510e39febec3ba3ddeb6838fb9cffa41e759
xpollen8 <github@xpollen8.com>
2023-07-17 08:14:33 -0700

    dw: initial checkin

---
+++ index.js
@@ -0,0 +1,65 @@
+import from './index.d';
+import mime from 'mime-types';
+
+const guessContentType = (filename: string): string => {
+	const ext = mime.lookup(filename);
+	return ext;
+}
+
+const fetchManifest = async ({ cache = true, root = '', recurse = false }:
+	{
+		cache?: boolean,
+		root?: string,
+		recurse?: boolean
+	}) : Promise<Manifest> => {
+
+	const manifest = await fetch(`${root}/manifest.json`,
+		{ cache: (cache) ? 'default' : 'no-store' })
+		.then(res => res.json())
+		.catch(err => { return { error: 'MISSING_MANIFEST' } });
+
+	/*
+		convert contents which is an object in the fetched data
+		into an array
+	 */
+	const new_manifest = { ...manifest };
+	new_manifest.root = root;		// add new entry
+	new_manifest.contents = [];	// replace .contents
+
+	manifest?.contents && await Promise.all(Object.keys(manifest?.contents)?.map(async key => {
+		const entry = manifest?.contents[key];
+		const externalLink = key.includes('https://');
+		let	contents = { name: key, link: (externalLink) ? key : `${root}/${key}` };
+		if (entry?.type === 'folder' && recurse) {
+			// fetch manifest for folder
+			contents = { type: 'folder', ...contents, ...await fetchManifest({ cache, root: `${root}/${key}`, recurse })};
+		} else if (key.includes('.json')) {
+			// fetch a json file
+			const body = await fetch(`${root}/${key}`,
+					{ cache: (cache) ? 'default' : 'no-store' })
+					.then(res => res.json())
+					.catch(err => { return { error: 'MISSING_MANIFEST' } });
+			contents = { ...contents, ...entry, type: entry.type || guessContentType(key) || 'application/octet-stream', json: { ...body } }
+		} else {
+			// else just add this entry
+			contents = { ...contents, ...entry, type: entry.type || guessContentType(key) || 'application/octet-stream' };
+		}
+		new_manifest.contents.push(contents);
+	}));
+
+	// sort entries - ideally w/errors on bottom below directories
+	new_manifest.contents = new_manifest.contents.sort((a: Obj, b: Obj) => {
+		const name1 = String(a.title || a.name);
+		const name2 = String(a.title || b.name);
+		//if (name1 === name2) {
+		//console.log("COMP", { a, b });
+			//if (a?.error && b?.error) return  ('' + name1).localeCompare(name2)
+			//if (a?.contents?.length && b?.contents?.length)  ('' + name1).localeCompare(name2)
+		//}
+		return ('' + name1).localeCompare(name2); 
+	});
+
+	return new_manifest;
+}
+
+module.exports = fetchManifest;
